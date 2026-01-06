8e584f6db9b660aaaebf85be1ad68d9dff990f26
Sujith <syjithkpy@gmail.com>
2026-01-06 05:57:18 +0000

    refactor: clean up spacing and comments in dependency injection file

--- backend/src/di/notes.di.ts
+++ backend/src/di/notes.di.ts
@@ -10,15 +10,18 @@ import Note_FindByIdUsecase from "../usecases/notes/findbyId";
 
 
 
+//db repository instance
 
 const noterepo=new NoteRepository();
 
 
-
+//usecase instances
 const cerateuseCase=new Note_CreateUseCase(noterepo);
 const noteFindUsecase=new Note_FindUsecase(noterepo);
 const noteFindByIdUsecase=new Note_FindByIdUsecase(noterepo);
 
+
+//controller instance
 
 const noteDI=new Note_Controller(
     cerateuseCase,
